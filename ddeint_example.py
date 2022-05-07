bee41af0ebd6e6363d2a80c9f8478b3dc11fca2a
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2022-05-07 11:16:34 +0530

    update dde example

--- ddeint_example.py
+++ ddeint_example.py
@@ -4,14 +4,21 @@ from ddeint import ddeint
 
 
 def model(XY, t, d):
+    """
+    We solve the following system:
+    X(t) = 1 (t < 0)
+    Y(t) = 2 (t < 0)
+    dX/dt = X * (1 - Y(t-d)) / 2
+    dY/dt = -Y * (1 - X(t-d)) / 2
+    """
     x, y = XY(t)
     xd, yd = XY(t - d)
     return np.array([0.5 * x * (1 - yd), -0.5 * y * (1 - xd)])
 
 
-past = lambda t: np.array([1 + t, 2 - t])  # 'history' at t<0
-tt = np.linspace(0, 30, 20000)  # times for integration
-d = 0.5  # set parameter d
+past = lambda t: np.array([1, 2])  # 'history' at t<0
+tt = np.linspace(2, 30, 20000)
+d = 0.2  # set parameter d
 yy = ddeint(model, tt, past, fargs=(d,))  # solve the DDE !
-plt.plot(tt, yy)
+plt.plot(yy[:, 0], yy[:, 1], lw=2, label=f"delay = {d:.01}")
 plt.show()
