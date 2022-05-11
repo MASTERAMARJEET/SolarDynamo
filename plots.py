b2d73c605548b966d1a7065d067a15207416d414
radha tanya <rrtanya2001@gmail.com>
2022-05-11 23:12:31 +0530

    Bifurcation for noise-less dynamo models

--- plots.py
+++ plots.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 from ddeint import ddeint
 from scipy.special import erf
 
-omega_by_L = -0.34
-alpha = 0.17
-tau = 15
-T0 = 2
-T1 = 0.5
+#omega_by_L = -0.34
+#alpha = 0.17
+tau = 1
+T0 =10
+T1 = 4
 Bmin = 1
 Bmax = 7
-N_D = alpha * omega_by_L * tau**2
+
 
 
 def past(t):
@@ -22,7 +22,7 @@ def quenching(B):
     return (1 + erf(B**2 - Bmin**2)) * (1 - erf(B**2 - Bmax**2)) * 0.25
 
 
-def model(Y, t):
+def model(Y, t,alpha):
     """
     `omega`: differential rotation
     `L`: length scale of the differential rotation
@@ -32,7 +32,7 @@ def model(Y, t):
     B_t, A_t = Y(t)
     _, A_T0 = Y(t - T0)
     B_T1, _ = Y(t - T1)
-    dB = omega_by_L * A_T0 - (B_t / tau)
+    dB = -2*alpha* A_T0 - (B_t / tau)
     dA = alpha * quenching(B_T1) * B_T1 - ( A_t / tau)
     return [dB, dA]
 
@@ -41,17 +41,24 @@ time_range = 1e3
 time_steps = 1
 cutoff = 100
 
-tt = np.linspace(0, time_range, int(time_range / time_steps + 1))
-yy = ddeint(model, tt, past)
+for i in range(70):
 
-fig, (ax1,ax2, ax3) = plt.subplots(3, 1, sharex=True)
-ax1.plot(tt[cutoff:], yy[cutoff:, 1], "r")
-ax1.set_ylabel("A", fontsize=14)
-ax2.plot(tt[cutoff:], yy[cutoff:, 0], "b")
-ax2.set_ylabel("$B_\phi$", fontsize=14)
-ax3.plot(tt[cutoff:], np.square(yy[cutoff:, 0]), "b")
-ax3.set_ylabel("$B_\phi^2$", fontsize=14)
-ax3.set_xlabel("Time", fontsize=14)
-fig.suptitle(f"$N_D$={N_D:0.2f}   $T_0$={T0}   $T_1$={T1}   $\omega$/L={omega_by_L}   $\\tau$={tau}")
-plt.tight_layout()
-plt.show()
+    #tau = tau1 + i*0.1
+    alpha = -1+0.05*i
+    N_D = -2*alpha **2 *tau**2 
+    tt = np.linspace(0, time_range, int(time_range / time_steps + 1))
+    yy = ddeint(model, tt, past, modelargs=(alpha,))
+
+    fig, (ax1,ax2, ax3) = plt.subplots(3, 1, sharex=True)
+    ax1.plot(tt[cutoff:], yy[cutoff:, 1], "r")
+    ax1.set_ylabel("A", fontsize=14)
+    ax2.plot(tt[cutoff:], yy[cutoff:, 0], "b")
+    ax2.set_ylabel("$B_\phi$", fontsize=14)
+    ax3.plot(tt[cutoff:], np.square(yy[cutoff:, 0]), "b")
+    ax3.set_ylabel("$B_\phi^2$", fontsize=14)
+    ax3.set_xlabel("Time", fontsize=14)
+    fig.suptitle(f"$N_D$={N_D:0.2f}   $T_0$={T0}   $T_1$={T1}   $\omega$/L={-2*alpha}   $\\tau$={tau}")
+    plt.tight_layout()
+    #plt.show()
+    plt.savefig(str(N_D)+'.png')
+    plt.clf
